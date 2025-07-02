6dab62a5977f33826a314e533bf5a74fd5da3d23
Samuel Roland <samuelrolandoumg@gmail.com>
2025-07-01 18:46:56 -0600

    Correccion de cors inyection

--- src/main/java/config/CorsConfig.java
+++ src/main/java/config/CorsConfig.java
@@ -23,10 +23,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") 
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://skynet-frontend-production.up.railway.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); 
+                        .allowCredentials(true);
             }
         };
     }
