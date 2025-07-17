c28c0b39173f699fb7b80e274313a6380b0a1da1
Samuel Roland <samuelrolandoumg@gmail.com>
2025-07-16 22:57:56 -0600

    reporte tecnico1

--- src/main/java/projection/DocumentosGeneradosProjection.java
+++ src/main/java/projection/DocumentosGeneradosProjection.java
@@ -12,7 +12,6 @@ package projection;
 public interface DocumentosGeneradosProjection {
     String getnombreDocumento();
     String geturlDocumento();
-    String getnombreDocumento();
     String getresultadoObtenido();
     String getfechaProgramada();
     String getfechaServicioFinalizada();
