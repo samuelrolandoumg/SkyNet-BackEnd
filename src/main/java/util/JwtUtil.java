7a1db49b655efdd0bef8e96ad644f3463dd2250f
Samuel Roland <samuelrolandoumg@gmail.com>
2025-07-27 16:15:53 -0600

    cambio de metodo de fecha

--- src/main/java/util/JwtUtil.java
+++ src/main/java/util/JwtUtil.java
@@ -19,13 +19,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "skynetSecretKey123"; // Cambiá por una segura
+    private final String SECRET_KEY = "skynetSecretKey123"; 
 
     public String generarToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 día
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) 
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
